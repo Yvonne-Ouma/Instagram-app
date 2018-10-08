1ffab276f298b9f3e04e1dd6f7648664827050b2
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-10-08 18:43:13 -0400

    profile displaying

--- instagram/urls.py
+++ instagram/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^',include('myig.urls')),
     
-    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
     
 ]
